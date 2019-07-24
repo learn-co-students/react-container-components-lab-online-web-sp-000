d1a54de158180b883021a4bd948389fc6c535b6e
jonmackeyparty <jon@jonmackey.party>
2019-07-24 16:18:10 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,16 @@
-// Code MovieReviews Here
+import React from 'react'
+
+
+const MovieReviews = ( {reviews} ) => (
+  <div className='review-list'>
+      {reviews.map(( review, index) =>
+        <div className='review' key={index} >
+          <h3><u>{review.headline}</u></h3>
+          <h4>{review.display_title} reviewed by {review.byline}</h4>
+          <p>{review.summary_short}</p><br />
+        </div>
+      )}
+  </div>
+)
+
+export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,8 +2,49 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
-            + `api-key=${NYT_API_KEY}`;
+const NYT_API_KEY = 'w34bOcousyNGkwjRfocEXYmB28aUlU67';
 
-// Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      reviews: [],
+      searchTerm: ''
+    }
+  }
+
+  handleChange = event => {
+    this.setState({
+      searchTerm: event.target.value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    this.searchMovies(this.state.searchTerm)
+  }
+
+  searchMovies = (searchTerm) => {
+    fetch(`https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=${searchTerm}&api-key=${NYT_API_KEY}`)
+    .then(response => response.json())
+    .then(data => {
+      this.setState({
+        reviews: data.results
+      })
+      console.log(data.results)
+    })
+  }
+
+  render() {
+    return(
+      <div className='searchable-movie-reviews'>
+        <form onSubmit={event => this.handleSubmit(event)}>
+          <input type='text' value={this.state.searchTerm} onChange={event => this.handleChange(event)} />
+          <button type='submit'>Search NYT Movies</button>
+        </form>
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    )
+  }
+}
