5f6276a977b9845d5f119c16a76ea93eee0a05fc
tiffanynuugen <tiffanynuugen@gmail.com>
2019-11-09 21:16:36 -0500

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,11 @@
-// Code MovieReviews Here
+import React from 'react';
+
+const MovieReviews = ({ display_title }) => {
+  return (
+    <ul className='review-list'>
+      <li className='review'>{display_title}</li>
+    </ul>
+  );
+};
+
+export default MovieReviews;
