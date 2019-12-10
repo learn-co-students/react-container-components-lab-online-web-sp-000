572a99c20c099a16f450ef5bde7f49854847ba26
John Hayes <JohnnyHayes7@gmail.com>
2019-12-10 14:21:54 -0500

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -2,8 +2,48 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
+const NYT_API_KEY = '2ilAGRIYLEJZ9xKYxmyDLZA7RW85GuIs';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+
+export default class LatestMovieReviewsContainer extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            reviews: []
+        }
+    }
+
+    componentDidMount() {
+        fetch(URL).then(resp => resp.json()).then( data => {
+            this.setState({
+                reviews: data.results
+            })
+        })
+
+    }
+
+    renderReviews = () => {
+        return this.state.reviews.map(lmr => {
+            
+            return(
+                < MovieReviews title={lmr.display_title} author={lmr.byline} image={lmr.multimedia.src} link_url={lmr.link.url} headline={lmr.headline} />  
+            ) 
+        })
+    }
+
+
+
+    render() { 
+        
+        return(
+            <div className="latest-movie-reviews">
+               {this.renderReviews()}
+            </div>
+        )
+    }
+}
+
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,8 +2,78 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
+const NYT_API_KEY = '2ilAGRIYLEJZ9xKYxmyDLZA7RW85GuIs';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+
+
+
+export default class SearchableMovieReviewsContainer extends Component {
+    constructor(){
+        super()
+        this.state = {
+            reviews: [],
+            searchTerm: ""
+        }
+    }
+
+    changeHandler = event => {
+        this.setState({
+            searchTerm: event.target.value
+        })
+        
+    }
+
+    submitHandler = event => {
+        event.preventDefault()
+        
+        fetch(URL + `&query=${this.state.searchTerm}`).then(resp => resp.json()).then(data => {
+            
+            this.setState({
+                reviews: data.results
+            })
+        })
+       
+    }
+
+
+    componentDidUpdate() {
+        this.renderReviews()
+    }
+   
+
+
+   renderReviews = () => {
+       
+       if(this.state.reviews.length > 0){
+           return this.state.reviews.map(review => {
+                 < MovieReviews title={review.display_title} author={review.byline} link_url={review.link.url} headline={review.headline} />
+            })
+        }
+    }
+
+    searchFieldCreator() {
+        return(
+            <form onSubmit={event => this.submitHandler(event)} onChange={event => this.changeHandler(event)}>
+                <label>Search Reviews: </label>
+                <input type="text" name="searchTerm"  />
+                <input type="submit" />
+            </form>
+
+
+            
+        )
+    }
+
+
+
+    render() {
+        return(
+            <div className='searchable-movie-reviews'>
+                {this.searchFieldCreator()}
+            </div>
+        )
+    }
+}
