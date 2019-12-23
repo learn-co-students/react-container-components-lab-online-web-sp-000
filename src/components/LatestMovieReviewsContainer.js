738ae6cc96160b1605d361822075feb0dd41d36f
github username <github email address>
2019-12-23 06:25:39 -0800

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,33 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+
+class LatestMovieReviewsContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reviews: null
+    }
+  }
+
+  componentDidMount = () => {
+    fetch(URL)
+      .then(resp => resp.json()).then(json => {this.updateState(json)});
+  }
+
+  updateState = (data) => {
+    this.setState({
+      reviews: data.results.slice(0, 10)
+    });
+  }
+
+  render() {
+    return(
+      <div className={'latest-movie-reviews'}>{this.state.reviews ? <MovieReviews reviews={this.state.reviews} /> : null}</div>
+    );
+  }
+}
+
+export default LatestMovieReviewsContainer;
+
+// https://api.nytimes.com/svc/movies/v2/reviews/all.json?api-key=dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ
--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,13 @@
 // Code MovieReviews Here
+import React from 'react';
+
+const MovieReviews = (props) => {return <div className={'review-list'}>{ props.reviews.map((r) => {
+  return <div className={'review'} key={r.link.url}>
+    <h2>{r.display_title} {r.mpaa_rating.length > 0 ? `- ${r.mpaa_rating}` : ''}</h2>
+    <h4>{r.headline}</h4>
+    <h5><em>{r.byline}</em></h5>
+    <a href={r.link.url}>Read Article</a>
+  </div>;
+})}</div>};
+
+export default MovieReviews;
