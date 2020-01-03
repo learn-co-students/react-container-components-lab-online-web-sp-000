0a4af4eea5c60a30dbbb5cab2f21a4d285eb5495
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-01-02 17:41:24 -0800

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,34 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+
+export default class LatestMovieReviewsContainer extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      reviews: []
+    }
+  }
+
+  componentDidMount() {
+    fetch(URL)
+      .then(response => response.json())
+      .then(reviews => {
+        // console.log(reviews.results)
+        this.setState({
+          reviews: reviews.results
+        })
+      })
+  }
+
+  render() {
+    return(
+      <div className="latest-movie-reviews">
+        <h1>Latest Movie Button</h1>
+        {<MovieReviews reviews={this.state.reviews}/>}
+      </div>
+    )
+  }
+
+}
--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,14 @@
 // Code MovieReviews Here
+import React from 'react'
+
+const MovieReviews = (props) => {
+  return(
+    <div className="review-list">
+      {props.reviews.map(review => <h3 className="review">{review.headline}</h3>)}
+    </div>
+  )
+}
+
+export default MovieReviews
+
+// 0: {display_title: "Advocate", mpaa_rating: "", critics_pick: 0, byline: "Ben Kenigsberg", headline: "‘Advocate’ Review: Defending More Than Just Her Clients", …}
