de592c1d3684bb4568b0b0881a4b80cceeedbe0b
atribecalledarty <arth3rs0ng@gmail.com>
2020-01-04 21:59:54 -0800

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,30 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+export default class LatestMovieReviewsContainer extends Component {
+    constructor(){
+        super()
+        this.state = {
+            reviews: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://api.nytimes.com/svc/movies/v2/reviews/all.json?api-key=smdVk4GGQkGPZja85sAk23KTtB77y3lX')
+            .then(resp => resp.json())
+            .then(json => {
+                this.setState({
+                    reviews: json.results
+                })
+            })
+    }
+    
+    render() {
+        return(
+            <div className="latest-movie-reviews">
+                {/* {console.log('hi')} */}
+                <MovieReviews reviews={this.state.reviews} />
+            </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import ReactDOM from 'react-dom';
 import LatestMovieReviewsContainer from './components/LatestMovieReviewsContainer';
 import SearchableMovieReviewsContainer from './components/SearchableMovieReviewsContainer';
 
+fetch('https://api.nytimes.com/svc/movies/v2/reviews/all.json?api-key=smdVk4GGQkGPZja85sAk23KTtB77y3lX')
+  .then(resp => resp.json())
+  .then(json => console.log(json));
+  
 ReactDOM.render(
   <div className="app">
     <SearchableMovieReviewsContainer />
