a39cc94df02a031a19b3791cfdf49d8b0815e7b4
chanwkkk <chanwk18@hotmail.com>
2020-02-21 17:20:20 -0700

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,17 @@
 // Code MovieReviews Here
+import React from 'react'
+   
+const renderReviews = (movies) => {
+  console.log(movies.movies[0])
+}
+
+const MovieReviews = (movies) =>{
+
+    return(
+        <div className="review-list">
+          {renderReviews(movies)}
+        </div>
+    )
+}
+
+export default MovieReviews
