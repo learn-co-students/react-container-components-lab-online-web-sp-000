00f184981587b1a03c8b0c030c02a4809c6054f1
Paul Rosero <paul_rosero@yahoo.com>
2020-04-09 20:54:08 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,13 @@
-// Code MovieReviews Here
+import React from 'react';
+import LatestMovieReviewsContainer from './LatestMovieReviewsContainer'
+import SearchableMovieReviewsContainer from './SearchableMovieReviewsContainer'
+
+const MovieReviews = ({reviews, index}) => ( 
+   
+    <div className={'review-list'} >
+        {/* console.log(reviews, "review") */}
+        { reviews.map(review => <LatestMovieReviewsContainer reviews={review.review} key={index} /> )}
+    </div>
+)
+
+export default MovieReviews; 
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -6,4 +6,27 @@ const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends Component {
+    state = {
+        reviews: {},
+        searchTerm: []
+    }
+
+    fetchData = () => {
+        fetch(URL)
+        .then(res => res.json())
+        .then(data => 
+            
+            this.setState({
+            reviews: data,
+            searchTerm: data
+            })
+        )
+    }
+
+    render(){
+        return(
+           <form className="searchable-movie-reviews" onSubmit={this.fetchData}></form>
+        )
+    }
+}
