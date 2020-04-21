3be7ee4ac10827a71a62dc813e5f5a1cb6debf4b
BlythePollard <github email address>
2020-04-20 21:12:31 -0600

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 const MovieReviews = ({reviews}) => (
+    //console.log(reviews)
     <div className="review-list">
     {reviews.map(review => 
         //key={review.id}
         <p>title={review.display_title}</p>
         
-        )}
+        )
+    }
     </div>
 )
 
