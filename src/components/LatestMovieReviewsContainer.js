f298dd0c3ebbb5572d4432b99ecffd95bb81cdf8
Soner Mezgitci <mezgitci9@gmail.com>
2020-08-17 10:37:57 -0400

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -2,8 +2,43 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
+
 const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends Component {
+ constructor(props){
+     super(props)
+     this.state= {
+    reviews: []
+
+     }
+ }
+ componentDidMount() {
+     fetch(URL)
+     .then(response => response.json())
+     .then(reviewData => this.setState ({reviews: reviewData.reviews}))
+     console.log(response)
+     
+     
+    }
+
+  renderReviews = () => {
+
+    return this.state.reviews.map(review => {
+        return(
+            <div> </div>
+        )
+    })
+  }
+  
+  render() {
+  return (
+      <div className= " "> {this.renderReviews} </div>
+  )
+  
+  }
+
+}
+export default LatestMovieReviewsContainer
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -6,4 +6,18 @@ const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends Component {
+constructor(props) {
+super(props)
+
+this.state = {
+    
+}
+
+
+}
+  componentDidMount() {
+  fetch(URL)
+
+  }
+}
