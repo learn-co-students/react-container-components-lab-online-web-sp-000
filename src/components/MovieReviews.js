53960c30c0367fdd931f6ddba3c801dc47327788
Jake Garcia <bballwiz4@gmail.com>
2020-08-22 17:15:56 -0700

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,17 @@
 // Code MovieReviews Here
+import React from 'react';
+
+const MovieReviews = ({reviews}) => {
+    return (
+        <div className="review-list">
+            {reviews.map(review => {
+                return <div className="review">
+                            <h3>{review.display_title}</h3>
+                            <p>{review.headline}</p>
+                        </div>
+            })}
+        </div>
+    )
+}
+
+export default MovieReviews;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -7,3 +7,45 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            reviews: [],
+            searchTerm: ""
+        }
+    }
+
+    handleInputChange = (e) => {
+        this.setState({
+            ...this.state.reviews,  
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const searchUrl = URL + `&query=${this.state.searchTerm}`
+        fetch(searchUrl)
+            .then(res => res.json())
+            .then(data => this.setState({reviews: data.results}))
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Searched Movie Reviews</h2>
+                <form onSubmit={e =>this.handleSubmit(e)}>
+                    <input name="searchTerm" value={this.state.searchTerm} onChange={e => this.handleInputChange(e)} />
+                    <button type="submit">Search</button>
+                </form>
+                <div>
+                    <h4>Results</h4>
+                    <MovieReviews reviews={this.state.reviews} />
+                </div>
+                
+            </div>
+        )
+    }
+}
