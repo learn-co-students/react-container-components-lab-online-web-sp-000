b8b1c79393c60ecf8d0c7cf4a1b3f987ca92b6ba
Nisticism <nistacular@gmail.com>
2020-08-31 17:17:32 -0700

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,9 @@
 // Code MovieReviews Here
+import React from 'react'
+const MovieReviews = ({reviews}) => {
+    <div className="review-list">
+        { reviews.map(review => <p className="review">{review.text}</p>) }
+    </div>
+}
+
+export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -7,3 +7,29 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+
+export default class SearchableMovieReviewsContainer extends React.Component {
+
+    constructor() {
+        super()
+        this.state = {
+            reviews: [],
+            search: ""
+        }
+    }
+
+    componentDidMount() {
+        fetch(`https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=<${this.state.search}>`+ `api-key=${NYT_API_KEY}`)
+      .then(response => response.json())
+      .then(reviewData => this.setState({ reviews: reviewData.reviews }))
+    }
+
+    render() {
+        return (
+            <input value="Search Movies Here" onChange={this.handleOnChange} />
+            <div className="searchable-movie-reviews">
+                <MovieReviews reviews={this.state.reviews} />
+            </div>
+        )
+    }
+}
