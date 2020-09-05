4de15cef197a8227427580015993b7e55077b001
mbade1 <mbade314@gmail.com>
2020-09-05 17:14:03 -0500

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -2,8 +2,32 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
+const NYT_API_KEY = 'EPgLNdBAFjRewiIiboJFHhkDsEwcyLpy';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code LatestMovieReviewsContainer Here
+export default class LatestMovieReviewsContainer extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            reviews: []
+        }
+    }
+    fetchReviews = () => {
+        fetch(URL)
+        .then(resp => resp.json())
+        .then(reviews => this.setState({ reviews: reviews.results }))
+    }
+
+    componentDidMount() {
+        this.fetchReviews()
+    }
+    render() {
+        return (
+            <div className="latest-movie-reviews">
+            <h2>Latest reviews</h2>
+            <MovieReviews reviews={this.state.reviews}/>
+          </div>
+        )
+    }
+}
--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,25 @@
-// Code MovieReviews Here
+import React from 'react';
+
+const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
+            + `api-key=${NYT_API_KEY}`;
+
+const MovieReviews = ({ reviews }) => {
+    return (
+        <div className='review-list'>
+            <ul>
+                {reviews.map(review=> 
+                    <div className='review'>
+                        <li>
+                            <h3>{review.headline}</h3>
+                            <h4>By: {review.byline}</h4>
+                            <p><b>Summary:</b> {review.summary_short}</p>
+                        </li>
+                    </div> 
+                )}
+            </ul>        
+        </div>
+    )
+}
+
+export default MovieReviews;
