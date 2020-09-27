e03a4e56819c31da800391c2fe0e22498f3753b3
github username <github email address>
2020-09-27 16:17:57 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,25 @@
 // Code MovieReviews Here
+
+import React from 'react'
+
+
+//presentational component
+//lists a series of movie reviews to the page 
+//stateless and functional
+//each review is wrapped by an element with class review 
+const MovieReviews = (props) => {
+    const listOfReviews =  props.reviews.map((review, index) => {
+        return <li className='review' key={index}>
+            <p> {review.display_title} </p> 
+            <p> {review.byline} </p>
+            <p> {review.summary_short} </p>
+        </li>
+    })
+    return (
+        <ol className="review-list">
+            {listOfReviews}
+        </ol>
+    )
+}
+
+export default MovieReviews
