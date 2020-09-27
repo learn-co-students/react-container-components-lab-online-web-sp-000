dff59d522f5d8572366fdce8df41d39db82d6f49
Alvin Coulson <aaacoulson@gmail.com>
2020-09-27 14:20:07 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,27 @@
-// Code MovieReviews Here
+import React from 'react';
+
+const MovieReviews = (props) => {
+    return (<div className="review-list">
+        {props.reviews.map((review, idx) => {
+        return <div key={idx} className="review">
+                <header>
+                    <a href="review.url">{review.display_title}</a>
+                    <span style={{marginLeft: "10px"}}> Author: {review.byline}</span>
+                </header>
+                <blockquote>{review.summary_short}</blockquote>
+            </div>
+        })}
+    </div>)
+}
+
+MovieReviews.defaultProps = {
+    reviews: []
+}
+
+// {
+//     display_title: "Adventures through the South!", 
+//     byline: "F. Froome",
+//     summary_short: "A thrilling narration of a young Eskimo experience on his journey through the South."
+// }
+
+export default MovieReviews;
