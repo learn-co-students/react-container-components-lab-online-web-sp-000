8a58818a8664f320d4a499bd0aed6f282e97bcb6
Cole Rau <colevrau@gmail.com>
2020-09-30 21:03:23 -0700

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,38 @@
 // Code MovieReviews Here
+
+import React from "react";
+
+//const renderReviews = (reviews) => {
+  //  reviews.map(review => {
+    //    <div className="review">{review.headline}</div>
+    //})
+//}
+
+const MovieReviews = (props) => {
+
+
+
+    let array = [];
+
+    if (props.allReviews && props.allReviews.map) {
+        array = props.allReviews.map(review => <div className="review">{review.headline}</div>)
+    }
+    
+
+
+    return (
+        <div className="review-list"> 
+            {array}
+        </div>
+    )
+}
+
+const displayReviews = (review) => {
+    return (
+        <div className="review">
+            {review.headline}
+        </div>
+    )
+}
+
+export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -7,3 +7,26 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends Component {
+    constructor() {
+        super();
+        this.state = null;
+    }
+
+    componentDidUpdate() {
+        // fix URL
+        fetch("https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=<your-api-key>&query=<search-term>")
+        .then(resp => resp.json())
+        .then(data => {
+
+        })
+    }
+
+    render() {
+        return(
+            <div className="searchable-movie-reviews">
+
+            </div>
+        )
+    }
+}
