380257a09739d9cc812047b9ea4757c22b8fec13
Cole Rau <colevrau@gmail.com>
2020-10-02 21:23:20 -0700

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -3,30 +3,20 @@
 import React from "react";
 
 const MovieReviews = (props) => {
-
-    let array = [];
-
-    if (props.allReviews && props.allReviews.map) {
-        array = props.allReviews.map(review => <div className="review">{review.headline}</div>)
-    }
-    if (props.searchReviews && props.searchReviews.map) {
-        array = props.searchReviews.map(review => <div className="review">{review.headline}</div>)
-    }
     
-
-
     return (
-        <div className="review-list"> 
-            {array}
+        
+        <div className="review-list">
+            {props.searchReviews.map((review, index) => <div className="review" key={index}>{review.headline}</div>)}
+
+            {props.allReviews.map((review, index) => <div className="review" key={index}>{review.headline}</div>)}
         </div>
     )
 }
 
-// const renderReviews = (reviews) => {
-//     reviews.map(review => {
-//         <div className="review">{review.headline}</div>
-//     })
-// }
-
+MovieReviews.defaultProps = {
+    allReviews: [],
+    searchReviews: []
+}
 
 export default MovieReviews
