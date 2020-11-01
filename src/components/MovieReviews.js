85a1fd98c24841f2fab1f01c13366a2728547a06
Christopher Dent <christopherdent01@gmail.com>
2020-11-01 14:47:53 -0500

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,41 @@
-// Code MovieReviews Here
+// Code presentation comp MovieReviews Here
+import React from "react";
+
+
+const Review = ({
+  headline,
+  byline,
+  link,
+  summary_short
+}) => {
+  return (
+
+    <div
+      key={headline}
+      className="review"
+    >
+      <header>
+        <a
+          className="review-link"
+          href={link.url}
+        >
+          {headline}
+        </a>
+        <br/>
+        <p className="author">{byline}</p>
+      </header>
+      <p>{summary_short}</p>
+    </div>
+  );
+};
+
+const MovieReviews = (props) => <div className= "review-list">{props.reviews.map(Review)}</div>
+
+
+
+
+MovieReviews.defaultProps = {
+ reviews: []
+ };
+
+export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -3,7 +3,64 @@ import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
 const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
-            + `api-key=${NYT_API_KEY}`;
+const BASE_URL =
+  'https://api.nytimes.com/svc/movies/v2/reviews/search.json?' +
+  `api-key=${NYT_API_KEY}&query=`;
 
-// Code SearchableMovieReviewsContainer Here
+
+
+class SearchableMovieReviewsContainer extends React.Component {
+  constructor() {
+  super()
+  this.state = {
+    searchTerm: "",
+    reviews: [] }
+  }
+
+  // componentDidMount(){
+  //  this.fetchReviews
+  //   }
+
+  fetchReviews = (searchTerm) =>{
+    fetch(BASE_URL.concat(this.state.searchTerm))
+    .then(resp => resp.json())
+    // .then(json => console.log(json.results))
+    .then(data => this.setState({ reviews: data.results }))
+
+  }
+
+  handleChange = event => {
+      this.setState({
+        searchTerm: event.target.value
+      })///remember can't access the new state until re-render
+    }
+
+
+componentDidUpdate(){
+    console.log(this.state.searchTerm)
+
+}
+
+handleSubmit = (event) =>{
+  event.preventDefault();
+  this.fetchReviews(this.state.searchTerm)
+  console.log(this.state.searchTerm)
+}
+
+  render() {
+     return(
+   <div>
+     <h1>Search Reviews</h1>
+
+      <form onSubmit= {event => this.handleSubmit(event)}>
+        <input type ="text" name="query" value={this.state.searchTerm} onChange={event => this.handleChange(event)}/>
+    </form>
+
+        <MovieReviews reviews={this.state.reviews} />
+ </div>
+     )
+   }
+
+}
+
+export default SearchableMovieReviewsContainer
