d8943b0c92a29da4183be98f27e4ce3bfefcd227
betodute <betodute@gmail.com>
2020-11-08 14:28:08 -0800

    Draft 1

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,23 @@
-// Code MovieReviews Here
+// Code MovieReviews Here: This Component Should be Stateless and Functional
+
+import React from 'react';
+
+const MovieReviews = ({reviews}) => {
+    return (<div className="review-list"> 
+        {reviews.map((review, index) => {
+            return (
+                <div className="review"> key="{index}">
+                    <h1> {review.display_title} </h1>
+                    <h2> {review.headline} </h2>
+                    <p> {review.summary_short} </p>
+                </div>
+            )
+        })}
+    </div>)
+}
+
+MovieReviews.defaultProps = {
+    reviews: []
+};
+
+export default MovieReviews
