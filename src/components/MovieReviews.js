a8a74848aea9f031d0c7f15c8d7f107663441a93
Dwaynew85 <mr_dwayne_w@yahoo.com>
2020-12-14 01:36:28 -0500

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -3,7 +3,13 @@ import React from 'react'
 export const MovieReviews = ({movies}) => {
     return (
         <div className="review-list">
-            { movies.map(movie => <div className="review" >movie</div>) }
+            { movies.map(movie => <div className="review" >
+                <h1>{movie.headline}</h1>
+                <h2>{movie.byline}</h2>
+                <a href={movie.link.url}>Site</a>
+                <p>{movie.summary_short}</p>
+                </div>
+            )}
         </div>
     )
 }
