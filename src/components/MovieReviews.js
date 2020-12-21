c4329f621c31e69d52992e98955d6bb84b5bb416
murphcoder <murphcoder@gmail.com>
2020-12-21 11:25:45 -0600

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,18 @@
 // Code MovieReviews Here
+import React from 'react';
+
+const MovieReviews = (props) => 
+    <ul className='review-list'>
+        {props.reviews.map(review => 
+            <li className='review' key={review.headline}>
+                Title : {review.display_title}<br />
+                MPAA Rating : {review.mpaa_rating}<br />
+                Critic's Pick : {review.critics_pick}<br />
+                <p>Summary : {review.summary_short}</p>
+                <a href={review.link.url}>{review.link.suggested_link_text}</a><br />
+            </li>
+        )}
+    </ul>
+    ;
+
+export default MovieReviews;
