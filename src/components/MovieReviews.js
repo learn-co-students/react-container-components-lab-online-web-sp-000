3c5e7bfbdf0ae985076c353043a5cbe58bfd0691
Drew Thompson <drewallynthompson@gmail.com>
2021-02-14 23:45:50 -0700

    Done

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,17 @@
-// Code MovieReviews Here
+import React, { PureComponent } from 'react';
+
+const MovieReviews = ({reviews}) => {
+    return(
+        <div className='review-list'>
+            {reviews.map((review, index) => {
+                return(
+                    <div className='review' key={index}>
+                        {review.display_title}
+                    </div>
+                )
+            })}
+        </div>
+    )
+
+}
+export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -7,3 +7,40 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends React.Component {
+    state={
+        reviews: [],
+        searchTerm: ''
+    }
+
+    handleChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        fetch(URL + `&query=${this.state.searchTerm}`)
+        .then(res => res.json())
+        .then(json => this.setState({reviews: json.results}))
+    }
+
+    render(){
+        return(
+            <div className='searchable-movie-reviews'>
+                <h1>Searchable Movie Results</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <input 
+                        type='text'
+                        name='searchTerm'
+                        value={this.state.searchTerm}
+                        onChange={this.handleChange}
+                         />
+                    <input type='submit' value='Search' />
+                </form>
+                < MovieReviews reviews={this.state.reviews} />
+            </div>
+        )
+    }
+}
