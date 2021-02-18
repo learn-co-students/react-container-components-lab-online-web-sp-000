3103be58d8aeeed70998c22e25f0aa412f37181e
SVRourke <samuelvanrourke@gmail.com>
2021-02-18 15:47:25 -0500

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,18 @@
 // Code MovieReviews Here
+import React from 'react'
+
+const ReviewItem = (rev) => {
+  return (
+    <li className='review'>
+      <h3>{rev.display_title}</h3>
+      <p>{rev.critics_pick}</p>
+    </li>
+  )
+}
+
+// stateless and functional.
+const MovieReviews = ({ reviews }) => {
+  return <ul className='review-list'>{reviews.map(ReviewItem)}</ul>
+}
+
+export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,9 +1,56 @@
 import React, { Component } from 'react';
-import 'isomorphic-fetch';
+// import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
-            + `api-key=${NYT_API_KEY}`;
+const NYT_API_KEY = 'pcdgU2vDjXe0iFJCPtQCagHMsmpueWca';
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?';
 
 // Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends Component {
+    constructor () {
+        super()
+        this.state = {
+            reviews: [],
+            searchTerm: ''
+        }
+    }
+    conditionalRender = () => {
+        console.log(this.state.reviews)
+        // return this.state.reviews.any > 0 ? <MovieReviews reviews={this.state.reviews} /> : <p>Search!</p>
+    }
+
+    submitHandler = event => {
+        event.preventDefault()
+        fetch(`${URL}query=${this.state.searchTerm}&api-key=${NYT_API_KEY}`)
+        .then(r => r.json())
+        .then(d => {
+            this.setState({
+                reviews: d.results
+            })
+        })
+    }
+
+    changeHandler = event => {
+        event.preventDefault()
+        this.setState({
+            searchTerm: event.target.value
+        })
+
+    }
+
+    render () {
+        return (
+            <div className='searchable-movie-reviews'>
+                <form onSubmit={this.submitHandler} >
+                    <input type='text' onChange={this.changeHandler} />
+                    <input type='submit' />
+                </form>
+
+                {/* {this.conditionalRender} */}
+
+                <MovieReviews reviews={this.state.reviews} />
+            </div>
+        )
+    }
+}
+export default SearchableMovieReviewsContainer
