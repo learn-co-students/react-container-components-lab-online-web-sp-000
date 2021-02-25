2665caa27f8c99f09a483ee50058fb8d19815a71
ismayton <ian.mayton@gmail.com>
2021-02-25 14:21:36 -0600

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,13 @@
-// Code MovieReviews Here
+import React from 'react'
+
+const MovieReviews = props => {
+    return <div className="review-list">
+        {props.reviews.map( review => 
+        <div className="review">
+            <h4>{review.headline}</h4>
+            <p>{review.summary_short}</p>
+        </div>)}
+    </div>
+}
+
+export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -6,4 +6,51 @@ const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends Component {
+    constructor() {
+        super()
+        this.state = {
+            searchTerm: '',
+            reviews: []
+        }
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            searchTerm: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        this.fetchReviews()
+    }
+
+    fetchReviews = () => {
+        let search = this.state.searchTerm.split(' ').join('+')
+        let url = `https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=t6JhQat86p8eAHNNZ9WHYJKDBIhBXPmt&query=${search}`
+        fetch(url)
+        .then(resp => resp.json())
+        .then(json => this.handleReviewState(json))
+        .catch(err => console.log(err))
+    }
+
+    handleReviewState = (json) => {
+        const array =  json.results.map( review => review )
+        if (array.length > 0) {
+        this.setState({ reviews: array })
+        } else {
+            console.log('No results found!')
+        }
+    }
+
+    render() {
+        return <div className="searchable-movie-reviews">
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" onChange={this.handleChange}/>
+                <input type="submit" value="Search Reviews" />
+            </form>
+            <MovieReviews reviews={this.state.reviews}/>
+        </div>
+    }
+}
