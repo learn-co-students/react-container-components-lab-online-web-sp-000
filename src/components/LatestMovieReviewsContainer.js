3bf1ecd98d97a3ab05e396e7172156fbce298c52
Sarah Condon <s.j.condon01@gmail.com>
2021-03-01 21:27:02 -0700

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,36 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+export default class LatestMovieReviewsContainer extends Component{
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            reviews: []
+        }
+    }
+
+    componentDidMount(){
+        this.fetchLastestMovieReview()
+    }
+
+    fetchLastestMovieReview() {
+        
+        fetch(URL)
+            .then(response => response.json())
+            .then(movieData => {
+                this.setState({
+                    reviews: movieData.results
+                })
+            })
+    }
+
+    render() {
+        return (
+            <div className="latest-movie-reviews">
+                <MovieReviews reviews={this.state.reviews}/>
+            </div>
+        )
+    }
+}
--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,25 @@
 // Code MovieReviews Here
+import React from 'react'
+
+const MovieReviews = (props) => {
+    const allMovies = props.reviews.map((review, index) => {
+        return (
+            <React.Fragment key={index}>
+                <li className="review">
+                    <h2>{review.display_title}</h2>
+                    <h3>{review.headline}</h3>
+                    <p>{review.summary_short}</p>
+                </li>
+            </React.Fragment>
+        )
+    })
+    return(
+        <div className="review-list">
+            <ol>
+                {allMovies}
+            </ol>
+        </div>
+    )
+}
+
+export default MovieReviews
