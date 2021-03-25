db74718e8ffb67250e8753d9cc6633f10baff5d9
Scott Hoisington <hoyzington@gmail.com>
2021-03-24 23:42:23 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,27 @@
-// Code MovieReviews Here
+import React from 'react'
+
+let liKey = 0
+
+const MovieReviews = props => {
+  return (
+    <ul className={'review-list'}>
+      {props.reviews.map(({ display_title, byline, link, headline, summary_short }) => {
+        liKey +=1
+        return (
+          <li className='review' key={liKey}>
+            <h2><b>{display_title}</b></h2>
+            <h4><i>{byline}</i></h4>
+            <h3>
+              <a href={link.url} target='_blank'>
+                {headline}
+              </a>
+            </h3>
+            <p>{summary_short}</p><br/>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,9 +1,48 @@
-import React, { Component } from 'react';
-import 'isomorphic-fetch';
+import React from 'react'
+import 'isomorphic-fetch'
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
-            + `api-key=${NYT_API_KEY}`;
+const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ'
+const URL = `https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=${NYT_API_KEY}&query=`
 
-// Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends React.Component {
+  constructor() {
+    super()
+    this.state = { searchTerm: '', reviews: [] }
+    this.input = React.createRef()
+  }
+
+  render() {
+    return (
+      <div className='searchable-movie-reviews'><br/>
+        <form onSubmit={this.fetchReviews}>
+          <input type="text" onChange={this.onChange} value={this.state.searchTerm} ref={this.input}/>
+          <input type="submit" value='Search'/>
+        </form>
+        <MovieReviews
+          reviews={this.state.reviews}
+          message={<b>Search for movie reviews</b>}
+        /><br/>
+      </div>
+    )
+  }
+
+  onChange = (e) => {
+    this.setState({ searchTerm: e.target.value })
+  }
+
+  fetchReviews = (e) => {
+    e.preventDefault()
+    fetch(URL + this.state.searchTerm)
+      .then(res => res.json())
+      .then(data => this.setState({ reviews: data.results }))
+    this.setState({ searchTerm: '' })
+    this.input.current.focus()
+  }
+
+  componentDidMount() {
+    this.input.current.focus()
+  }
+}
+
+export default SearchableMovieReviewsContainer
