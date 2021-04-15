352dc778503de9210c1dc00d31d2c472ae01f62a
RSigmaSG <sg.comparch@gmail.com>
2021-04-15 01:12:42 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,13 @@
 // Code MovieReviews Here
+import React, { Component } from 'react';
+
+const MovieReviews = props => {
+    const Review = ({ display_title }) => <li className="review">{display_title}</li>;
+    return (
+        <div className="review-list">
+            {props.reviews.map(Review)}
+        </div>
+      )
+    }
+
+  export default MovieReviews;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -7,3 +7,59 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends Component {
+    constructor() {
+        super();
+    
+        this.state = {
+            searchTerm: "",
+            reviews: []
+        };
+      }
+    componentDidMount() {
+        this.fetchURLs()
+      }
+    
+      fetchURLs = () => {
+        fetch(`https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=Vf5Peb4vdEoDQikrNw8QuGKe6O4NaqmD&query=${this.state.searchTerm}`)
+        .then(response => response.json())
+          .then(reviewData => {
+            this.setState({
+              reviews: reviewData.results.map(review => review)
+            })
+          })
+      }
+   
+      handleClick = (event) => {
+        event.preventDefault();
+        this.fetchURLs(this.state.searchTerm)
+        }
+
+        handleChange = (event) => {
+            const value = event.target.value;
+            this.setState({
+                searchTerm: value
+            });
+        };
+        
+        render() {
+    
+
+            return (
+                <div className="latest-movie-reviews">
+                <form onSubmit={this.handleClick}>
+                    <input value={this.state.searchTerm} onChange={this.handleChange}/>
+                    <button>Find Movie</button>
+
+                </form>
+                
+                    <p> Movies:</p>
+                    <MovieReviews reviews={this.state.reviews}/>
+                </div>
+                
+            
+            )
+        }
+    }
+  
+  export default SearchableMovieReviewsContainer;
